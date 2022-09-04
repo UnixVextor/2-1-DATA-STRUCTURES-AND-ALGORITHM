8b4272e4c00c16dc0085093ba2389ea9576617dc
UnixVextor <vitoon.sdf@gmail.com>
2022-09-04 09:53:15 +0700

    fix item 5 parameter of function more than 2

--- CH6/ch6_5.py
+++ CH6/ch6_5.py
@@ -5,24 +5,31 @@
 
 หมายเหตุ ฟังก์ชันมี parameter ได้ไม่เกิน 2 ตัว
 '''
-def Draw(num,i = 0,j=0):
-    if num > 0: 
+    
+def Draw(i = 0,j=0):
+    global inp
+    if inp > 0:  
         print('_' * i,end='')
-        print('#' * j)
-        Draw(num-1,i-1,j+1)
-    elif num < 0:
-        print('_' * i,end='')
-        print('#' * j)
-        Draw(num+1,i+1,j-1)
+        print('#' * j)    
+        inp -= 1
+        Draw(i-1,j+1)
 
+    elif inp < 0:
+        print('_' * i,end='')
+        print('#' * j)
+        inp += 1
+        Draw(i+1,j-1)
+
+global i
+global j
 inp = int(input('Enter Input : '))
 if inp > 0:
     i = inp - 1
     j = 1 
-    Draw(inp,i,j)
+    Draw(i,j)
 elif inp < 0:
     i = 0
     j = (inp * -1)
-    Draw(inp,i,j)
+    Draw(i,j)
 else:
     print('Not Draw!')
